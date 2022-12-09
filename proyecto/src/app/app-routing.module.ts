c2158ce56871119175615f31bf7984496bc7e500
luispinzon <luis.pinzon@cetys.edu.mx>
2022-12-08 16:05:39 -0800

    final

--- proyecto/src/app/app-routing.module.ts
+++ proyecto/src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomePageComponent } from './Components/home-page/home-page.component';
+import { ClienteComponent } from './Components/cliente/cliente.component';
+import { NegocioComponent } from './Components/negocio/negocio.component';
+import { InformacionComponent } from './Components/informacion/informacion.component';
+import { ComentariosComponent } from './Components/comentarios/comentarios.component';
+import { SesionComponent } from './Components/sesion/sesion.component';
+import { RegistroComponent } from './Components/registro/registro.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+{path: '', component: HomePageComponent},
+{path: 'cliente', component: ClienteComponent},
+{path: 'negocio', component: NegocioComponent},
+{path: 'informacion', component: InformacionComponent},
+{path: 'comentarios', component: ComentariosComponent},
+{path: 'sesion', component: SesionComponent},
+{path: 'registro', component: RegistroComponent},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- proyecto/src/app/app.module.ts
+++ proyecto/src/app/app.module.ts
@@ -3,11 +3,31 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './Components/footer/footer.component';
+import { HeaderComponent } from './Components/header/header.component';
+import { HomePageComponent } from './Components/home-page/home-page.component';
+import { LandingPageComponent } from './Components/landing-page/landing-page.component';
+import { ClienteComponent } from './Components/cliente/cliente.component';
+import { NegocioComponent } from './Components/negocio/negocio.component';
+import { InformacionComponent } from './Components/informacion/informacion.component';
+import { ComentariosComponent } from './Components/comentarios/comentarios.component';
+import { SesionComponent } from './Components/sesion/sesion.component';
+import { RegistroComponent } from './Components/registro/registro.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    HeaderComponent,
+    HomePageComponent,
+    LandingPageComponent,
+    ClienteComponent,
+    NegocioComponent,
+    InformacionComponent,
+    ComentariosComponent,
+    SesionComponent,
+    RegistroComponent
   ],
   imports: [
     BrowserModule,
